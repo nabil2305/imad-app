34b937d0dbb08f440d3f9864075b6120c007d32f
nabil2305 <36496865+nabil2305@users.noreply.github.com>
2018-04-01 14:24:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').POOL;
+
+var config = {
+    user:'nabil2305mahtab',
+    database:'nabil2305mahtab',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ 
+});
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err)
+        { res.status(500).send(err.toString());
+    }
+    else{
+        res.send(JSON.stringify(result));
+    }
+    });
+    
 });
 
 app.get('/article-one', function(req,res) {
